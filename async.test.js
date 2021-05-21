28455b8c00d443f7332a4c2a5e9b23a9a1e9b879
Gabriel Rocha <gabriel.altair.rocha@gmail.com>
2021-05-20 22:54:30 -0400

    feat: test promise with resolves/rejects

--- async.test.js
+++ async.test.js
@@ -1,6 +1,7 @@
-// callback
+// callbacks
 
-// the done function must be used to test callbacks
+// done() must be used to test callbacks
+// try/catch block is needed to log the failure reason (otherwise will be a timeout)
 
 test("Data should be coffee", (done) => {
   const item = "coffee";
@@ -15,7 +16,50 @@ test("Data should be coffee", (done) => {
   }
 
   setTimeout(() => {
-    console.log(item);
     return callback(item);
-  }, 3000);
+  }, 1000);
+});
+
+// promises
+
+const asyncFunction = async (item) => {
+  return new Promise((resolve, reject) => {
+    try {
+      if (item === "tea") {
+        resolve(item);
+      } else {
+        throw new Error("Data received is not tea");
+      }
+    } catch (error) {
+      reject(error);
+    }
+  });
+};
+
+test("Data should be tea", async () => {
+  const data = "tea";
+
+  return asyncFunction(data).then((res) => {
+    expect(res).toBe("tea");
+  });
+});
+
+test("Should throw if data is not tea", async () => {
+  const data = "not-tea";
+
+  return asyncFunction(data).catch((rej) => {
+    expect(rej).toBeInstanceOf(Error);
+  });
+});
+
+test("Test happy path with resolves", async () => {
+  const data = "tea";
+
+  return expect(asyncFunction(data)).resolves.toBe("tea");
+});
+
+test("Test exception with rejects", async () => {
+  const data = "not-tea";
+
+  return expect(asyncFunction(data)).rejects.toBeInstanceOf(Error);
 });
